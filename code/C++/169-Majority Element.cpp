2c1ddae287970a1889c10d36574f04ba1697935f
Chen Kai <kakachen1990@gmail.com>
2022-02-09 17:23:36 +0800

    20220209

--- code/C++/169-Majority Element.cpp
+++ code/C++/169-Majority Element.cpp
@@ -18,29 +18,64 @@
 
 // n == nums.length
 // 1 <= n <= 5 * 104
-// -231 <= nums[i] <= 231 - 1
+// -2^31 <= nums[i] <= 2^31 - 1
  
 
 // Follow-up: Could you solve the problem in linear time and in O(1) space?
 
 class Solution {
+// 快速排序写法
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        int threshhold = nums.size() / 2;
-        int ans;
-        for (int num: nums) {
-            freq[num]++;
-            if (freq[num] > threshhold) {
-                ans = num;
-                break;
-            }
-        }
-        return ans;
+        quickSort(nums);
+        return nums[nums.size() / 2];
     }
 
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums);
+        return nums;
+    }
+
+    void quickSort(vector<int>& nums) {
+        quickSort(nums, 0, nums.size() - 1);
+    }
+
+    void quickSort(vector<int>& nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = partition(nums, start, end);
+        quickSort(nums, start, mid - 1);
+        quickSort(nums, mid + 1, end);
+    }
+
+    int partition(vector<int>& nums, int start, int end) {
+        int pivot = nums[start];
+        int left = start + 1;
+        int right = end;
+        while (left < right) {
+            while (left < right && nums[left] <= pivot) {
+                left ++;
+            }
+            if (left != right) {
+                swap(nums[left], nums[right]);
+                right --;
+            }
+        }
+        if (left == right && nums[right] > pivot) {
+            right --;
+        }
+        if  (right != start) {
+            swap(nums[start], nums[right]);
+        }
+        return right;
+    }
+
+
+
+    // 偷懒写法
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        return nums[nums.size() / 2];
     }
 };
