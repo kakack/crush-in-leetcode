835c1d4b34e302bf24a3249380405403c51b7cc6
kakack <kakachen1990@gmail.com>
2022-03-09 23:06:48 +0800

    20220309

---
+++ code/684-Redundant Connection/684-Redundant Connection.c
@@ -0,0 +1,74 @@
+/*
+In this problem, a tree is an undirected graph that is connected and has no cycles.
+
+The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N), with one additional edge added.
+The added edge has two different vertices chosen from 1 to N, and was not an edge that already existed.
+
+The resulting graph is given as a 2D-array of edges.
+Each element of edges is a pair [u, v] with u < v, that represents an undirected edge connecting nodes u and v.
+
+Return an edge that can be removed so that the resulting graph is a tree of N nodes.
+If there are multiple answers, return the answer that occurs last in the given 2D-array.
+The answer edge [u, v] should be in the same format, with u < v.
+
+Example 1:
+Input: [[1,2], [1,3], [2,3]]
+Output: [2,3]
+Explanation: The given undirected graph will be like this:
+  1
+ / \
+2 - 3
+Example 2:
+Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
+Output: [1,4]
+Explanation: The given undirected graph will be like this:
+5 - 1 - 2
+    |   |
+    4 - 3
+Note:
+The size of the input 2D-array will be between 3 and 1000.
+Every integer represented in the 2D-array will be between 1 and N, where N is the size of the input array.
+
+Update (2017-09-26):
+We have overhauled the problem description + test cases and specified clearly the graph is an undirected graph.
+For the directed graph follow up please see Redundant Connection II). We apologize for any inconvenience caused.
+
+*/
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int find(int * f, int x) {
+    if (f[x] == x) {
+        return x;
+    }
+    f[x] = find(f, f[x]);
+    return f[x];
+}
+
+void unioned(int * f, int x, int y) {
+    int fx = find(f, x);
+    int fy = find(f, y);
+    f[fx] = fy;
+}
+
+int* findRedundantConnection(int** edges, int edgesSize, int* edgesColSize, int* returnSize){
+    int* f = (int*)malloc(sizeof(int) * (edgesSize + 1));
+    for (int i = 0; i <= edgesSize; i ++) {
+        f[i] = i;
+    }
+    for (int i = 0; i < edgesSize; i ++) {
+        int node1 = edges[i][0];
+        int node2 = edges[i][1];
+        if (find(f, node1) != find(f, node2)) {
+            unioned(f, node1, node2);
+        } else {
+            *returnSize = 2;
+            return edges[i];
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
