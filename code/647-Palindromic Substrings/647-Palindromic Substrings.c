5fffa473e284f4a17bd3da86ea971dfc3a4b609f
kakack <kakachen1990@gmail.com>
2022-05-22 15:14:04 +0800

    20220522

---
+++ code/647-Palindromic Substrings/647-Palindromic Substrings.c
@@ -0,0 +1,41 @@
+Given a string s, return the number of palindromic substrings in it.
+
+// A string is a palindrome when it reads the same backward as forward.
+
+// A substring is a contiguous sequence of characters within the string.
+
+ 
+
+// Example 1:
+
+// Input: s = "abc"
+// Output: 3
+// Explanation: Three palindromic strings: "a", "b", "c".
+// Example 2:
+
+// Input: s = "aaa"
+// Output: 6
+// Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consists of lowercase English letters.
+
+
+
+
+int countSubstrings(char * s){
+    int n = strlen(s), res = 0;
+    for (int i  = 0; i < 2 * n - 1; i ++) {
+        int l = i / 2;
+        int r = i / 2 + i % 2;
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            l --;
+            r ++;
+            res ++;
+        }
+    }
+    return res;
+}
