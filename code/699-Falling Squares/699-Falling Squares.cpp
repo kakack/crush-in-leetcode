346ad651081182bc98e2c8b8c43acb026f8a932c
Chen Kai <kakachen1990@gmail.com>
2022-05-26 17:02:05 +0800

    20220526

--- code/699-Falling Squares/699-Falling Squares.cpp
+++ code/699-Falling Squares/699-Falling Squares.cpp
@@ -37,29 +37,24 @@
 // 1 <= lefti <= 10^8
 // 1 <= sideLengthi <= 10^6
 
-
-
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-
-int* fallingSquares(int** positions, int positionsSize, int* positionsColSize, int* returnSize){
-    int *heights = (int*)malloc(sizeof(int) * positionsSize);
-    for (int i = 0; i < positionsSize; i ++) {
-        int left1 = positions[i][0], right1 = positions[i][0] + positions[i][1] - 1;
-        heights[i] = positions[i][1];
-        for (int j = 0; j < i; j ++) {
-            int left2 = positions[j][0], right2 = positions[j][0] + positions[j][1] - 1;
-            if (right1 >= left2 && right2 >= left1) {
-                heights[i] = MAX(heights[i], heights[j] + positions[i][1]);
+class Solution {
+public:
+    vector<int> fallingSquares(vector<vector<int>>& positions) {
+        int n = positions.size();
+        vector<int> heights(n);
+        for (int i = 0; i < n; i ++) {
+            int left1 = positions[i][0], right1 = positions[i][0] + positions[i][1] - 1;
+            heights[i] = positions[i][1];
+            for (int j = 0; j < i; j ++) {
+                int left2 = positions[j][0], right2 = positions[j][0] + positions[j][1] - 1;
+                if (right1 >= left2 && right2 >= left1) {
+                    heights[i] = max(heights[i], heights[j] + positions[i][1]);
+                }
             }
         }
+        for (int i = 1; i < n; i ++) {
+            heights[i] = max(heights[i], heights[i - 1]);
+        }
+        return heights;
     }
-    for (int i = 1; i < positionsSize; i++) {
-        heights[i] = MAX(heights[i], heights[i - 1]);
-    }
-    *returnSize = positionsSize;
-    return heights;
-}
+};
