a39decd37088a58c83801b740af59d8b0ebebaa5
Chen Kai <kakachen1990@gmail.com>
2022-05-30 15:35:32 +0800

    20220530

--- code/1022-Sum of Root To Leaf Binary Numbers/1022-Sum of Root To Leaf Binary Numbers.cpp
+++ code/1022-Sum of Root To Leaf Binary Numbers/1022-Sum of Root To Leaf Binary Numbers.cpp
@@ -28,22 +28,28 @@
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 
-int dfs(struct TreeNode* root, int val) {
-    if (!root) {
-        return 0;
+class Solution {
+private:
+    int dfs(TreeNode* node, int val) {
+        if (!node) {
+            return 0;
+        }
+        val = (val << 1) | node->val;
+        if (!node->left && !node->right) {
+            return val;
+        }
+        return dfs(node->left, val) + dfs(node->right, val);
     }
-    val = (val << 1) | root->val;
-    if (!root->left && !root->right) {
-        return val;
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
     }
-    return dfs(root->left, val) + dfs(root->right, val);
-}
-
-int sumRootToLeaf(struct TreeNode* root){
-    return dfs(root, 0);
-}
+};
