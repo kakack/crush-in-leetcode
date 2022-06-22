ff17770f0918362085f127c00ad7b264d82b9614
Chen Kai <kakachen1990@gmail.com>
2022-06-22 15:30:46 +0800

    20220622

---
+++ code/513-Find Bottom Left Tree Value/513-Find Bottom Left Tree Value.py
@@ -0,0 +1,42 @@
+# Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [2,1,3]
+# Output: 1
+# Example 2:
+
+
+# Input: root = [1,2,3,4,null,5,6,null,null,7]
+# Output: 7
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [1, 104].
+# -2^31 <= Node.val <= 2^31 - 1
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        curVal = curHeight = 0
+        def dfs(node: Optional[TreeNode], height: int) -> None:
+            if node is None:
+                return
+            height += 1
+            dfs(node.left, height)
+            dfs(node.right, height)
+            nonlocal curVal, curHeight
+            if height > curHeight:
+                curHeight = height
+                curVal = node.val
+        dfs(root, 0)
+        return curVal
