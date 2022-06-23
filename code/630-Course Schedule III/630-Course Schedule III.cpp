8e8011657a1d93d7b1e683f6d9ca0828fd077309
Chen Kai <kakachen1990@gmail.com>
2022-06-23 10:13:18 +0800

    20220623

--- code/630-Course Schedule III/630-Course Schedule III.cpp
+++ code/630-Course Schedule III/630-Course Schedule III.cpp
@@ -1,1 +1,60 @@
+// There are n different online courses numbered from 1 to n. You are given an array courses where courses[i] = [durationi, lastDayi] indicate that the ith course should be taken continuously for durationi days and must be finished before or on lastDayi.
 
+// You will start on the 1st day and you cannot take two or more courses simultaneously.
+
+// Return the maximum number of courses that you can take.
+
+ 
+
+// Example 1:
+
+// Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+// Output: 3
+// Explanation: 
+// There are totally 4 courses, but you can take 3 courses at most:
+// First, take the 1st course, it costs 100 days so you will finish it on the 100th day, and ready to take the next course on the 101st day.
+// Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and ready to take the next course on the 1101st day. 
+// Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day. 
+// The 4th course cannot be taken now, since you will finish it on the 3300th day, which exceeds the closed date.
+// Example 2:
+
+// Input: courses = [[1,2]]
+// Output: 1
+// Example 3:
+
+// Input: courses = [[3,2],[4,3]]
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= courses.length <= 10^4
+// 1 <= durationi, lastDayi <= 10^4
+
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), [](const auto& c0, const auto& c1) {
+            return c0[1] < c1[1];
+        });
+
+        priority_queue<int> q;
+        // 优先队列中所有课程的总时间
+        int total = 0;
+
+        for (const auto& course: courses) {
+            int ti = course[0], di = course[1];
+            if (total + ti <= di) {
+                total += ti;
+                q.push(ti);
+            }
+            else if (!q.empty() && q.top() > ti) {
+                total -= q.top() - ti;
+                q.pop();
+                q.push(ti);
+            }
+        }
+
+        return q.size();
+    }
+};
