3923bc09c010c045654e51e319e89fd4dddbbed5
Chen Kai <kakachen1990@gmail.com>
2022-06-29 14:50:57 +0800

    20220629

--- code/41-First Missing Positive/41-First Missing Positive.cpp
+++ code/41-First Missing Positive/41-First Missing Positive.cpp
@@ -11,36 +11,55 @@ Your algorithm should run in O(n) time and uses constant space.
 
 */
 
+
 class Solution {
 public:
-    int firstMissingPositive(int A[], int n) {
-        int i =0, max=0,j=0;
-     int flag=0;
-     int missingPositive=0;
-     for(i=0;i<n;i++)
-     {
-         if(A[i]>max)
-             max = A[i];
-     }
-     if(max==1)
-         return 2;
-     for(i=1;i<max;i++)
-     {
-         for(j=0;j<n;j++)
-             {
-                 flag=0;
-                if(A[j]==i)
-                    {
-                    flag=1;
-                    break;
-                    }
-             }
-         if(flag!=1)
-             return i;
-
-     }
-
-     return max+1;
-        
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i ++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
+            }
+        }
+        for (int i = 0; i < n; i ++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 };
+
+// class Solution {
+// public:
+//     int firstMissingPositive(int A[], int n) {
+//         int i =0, max=0,j=0;
+//      int flag=0;
+//      int missingPositive=0;
+//      for(i=0;i<n;i++)
+//      {
+//          if(A[i]>max)
+//              max = A[i];
+//      }
+//      if(max==1)
+//          return 2;
+//      for(i=1;i<max;i++)
+//      {
+//          for(j=0;j<n;j++)
+//              {
+//                  flag=0;
+//                 if(A[j]==i)
+//                     {
+//                     flag=1;
+//                     break;
+//                     }
+//              }
+//          if(flag!=1)
+//              return i;
+
+//      }
+
+//      return max+1;
+        
+//     }
+// };
