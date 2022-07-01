a4b88568227c2183bb2dd0fd32b5134a5c20bb0f
Chen Kai <kakachen1990@gmail.com>
2022-07-01 09:53:49 +0800

    20220701

--- code/241-Different Ways to Add Parentheses/241-Different Ways to Add Parentheses.cpp
+++ code/241-Different Ways to Add Parentheses/241-Different Ways to Add Parentheses.cpp
@@ -1,1 +1,88 @@
+// Given a string expression of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. You may return the answer in any order.
 
+// The test cases are generated such that the output values fit in a 32-bit integer and the number of different results does not exceed 104.
+
+ 
+
+// Example 1:
+
+// Input: expression = "2-1-1"
+// Output: [0,2]
+// Explanation:
+// ((2-1)-1) = 0 
+// (2-(1-1)) = 2
+// Example 2:
+
+// Input: expression = "2*3-4*5"
+// Output: [-34,-14,-10,-10,10]
+// Explanation:
+// (2*(3-(4*5))) = -34 
+// ((2*3)-(4*5)) = -14 
+// ((2*(3-4))*5) = -10 
+// (2*((3-4)*5)) = -10 
+// (((2*3)-4)*5) = 10
+ 
+
+// Constraints:
+
+// 1 <= expression.length <= 20
+// expression consists of digits and the operator '+', '-', and '*'.
+// All the integer values in the input expression are in the range [0, 99].
+
+class Solution {
+public:
+    const int ADDITION = -1;
+    const int SUBTRACTION = -2;
+    const int MULTIPLICATION = -3;
+
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> ops;
+        for (int i = 0; i < expression.size();) {
+            if (!isdigit(expression[i])) {
+                if (expression[i] == '+') {
+                    ops.push_back(ADDITION);
+                } else if (expression[i] == '-') {
+                    ops.push_back(SUBTRACTION);
+                } else {
+                    ops.push_back(MULTIPLICATION);
+                }
+                i++;
+            } else {
+                int t = 0;
+                while (i < expression.size() && isdigit(expression[i])) {
+                    t = t * 10 + expression[i] - '0';
+                    i++;
+                }
+                ops.push_back(t);
+            }
+        }
+        vector<vector<vector<int>>> dp((int) ops.size(), vector<vector<int>>((int) ops.size()));
+        for (int i = 0; i < ops.size(); i += 2) {
+            dp[i][i] = {ops[i]};
+        }
+        for (int i = 3; i <= ops.size(); i++) {
+            for (int j = 0; j + i <= ops.size(); j += 2) {
+                int l = j;
+                int r = j + i - 1;
+                for (int k = j + 1; k < r; k += 2) {
+                    auto& left = dp[l][k - 1];
+                    auto& right = dp[k + 1][r];
+                    for (auto& num1 : left) {
+                        for (auto& num2 : right) {
+                            if (ops[k] == ADDITION) {
+                                dp[l][r].push_back(num1 + num2);
+                            }
+                            else if (ops[k] == SUBTRACTION) {
+                                dp[l][r].push_back(num1 - num2);
+                            }
+                            else if (ops[k] == MULTIPLICATION) {
+                                dp[l][r].push_back(num1 * num2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][(int) ops.size() - 1];
+    }
+};
