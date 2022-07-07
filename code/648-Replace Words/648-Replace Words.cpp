0c34d74460d4a26314b9285e799e2590e486d013
Chen Kai <kakachen1990@gmail.com>
2022-07-07 09:52:46 +0800

    20220707

--- code/648-Replace Words/648-Replace Words.cpp
+++ code/648-Replace Words/648-Replace Words.cpp
@@ -1,1 +1,75 @@
+// In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word successor. For example, when the root "an" is followed by the successor word "other", we can form a new word "another".
 
+// Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the successors in the sentence with the root forming it. If a successor can be replaced by more than one root, replace it with the root that has the shortest length.
+
+// Return the sentence after the replacement.
+
+ 
+
+// Example 1:
+
+// Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+// Output: "the cat was rat by the bat"
+// Example 2:
+
+// Input: dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs"
+// Output: "a a b c"
+ 
+
+// Constraints:
+
+// 1 <= dictionary.length <= 1000
+// 1 <= dictionary[i].length <= 100
+// dictionary[i] consists of only lower-case letters.
+// 1 <= sentence.length <= 10^6
+// sentence consists of only lower-case letters and spaces.
+// The number of words in sentence is in the range [1, 1000]
+// The length of each word in sentence is in the range [1, 1000]
+// Every two consecutive words in sentence will be separated by exactly one space.
+// sentence does not have leading or trailing spaces.
+
+class Solution {
+public:
+    vector<string_view> split(string &str, char ch) {
+        int pos = 0;
+        int start = 0;
+        string_view s(str);
+        vector<string_view> ret;
+        while (pos < s.size()) {
+            while (pos < s.size() && s[pos] == ch) {
+                pos++;
+            }
+            start = pos;
+            while (pos < s.size() && s[pos] != ch) {
+                pos++;
+            }
+            if (start < s.size()) {
+                ret.emplace_back(s.substr(start, pos - start));
+            }
+        }
+        return ret;
+    }
+
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        unordered_set<string_view> dictionarySet;
+        for (auto &root : dictionary) {
+            dictionarySet.emplace(root);
+        }
+        vector<string_view> words = split(sentence, ' ');
+        for (auto &word : words) {
+            for (int j = 0; j < word.size(); j++) {
+                if (dictionarySet.count(word.substr(0, 1 + j))) {
+                    word = word.substr(0, 1 + j);
+                    break;
+                }
+            }
+        }
+        string ans;
+        for (int i = 0; i < words.size() - 1; i++) {
+            ans.append(words[i]);
+            ans.append(" ");
+        }
+        ans.append(words.back());
+        return ans;
+    }
+};
